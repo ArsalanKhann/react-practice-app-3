6a15528ef9458dfd45ebfac74db197315b82af66
Arsalan Khan <arsalankhan3108@gmail.com>
2021-02-16 01:33:26 +0500

    30% errors

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import Menu from "./Menu";
 import Categories from "./Categories";
 import items from "./data";
 
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+console.log(allCategories);
+
+const filterItems = (category) => {
+  const newItems = items.filter((item) => item.category === category);
+  setMenuItems(newItems);
+};
+
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState([allCategories]);
 
   return (
     <main>
@@ -14,7 +22,7 @@ function App() {
           <h2>Menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories />
+        <Categories filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
