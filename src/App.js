31664b6bde2032c3a664260b509530b1e2a0385a
Arsalan Khan <arsalankhan3108@gmail.com>
2021-02-18 16:59:07 +0500

    60% errors

--- src/App.js
+++ src/App.js
@@ -6,14 +6,18 @@ import items from "./data";
 const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 console.log(allCategories);
 
-const filterItems = (category) => {
-  const newItems = items.filter((item) => item.category === category);
-  setMenuItems(newItems);
-};
-
 function App() {
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState([allCategories]);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
 
   return (
     <main>
